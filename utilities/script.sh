a868ee59da4c503e77eff2646d279f7a50a19582
Shete <varsha-vishwas.shete@capgemini.com>
2023-02-10 18:11:41 +0530

    jenkins

--- utilities/script.sh
+++ utilities/script.sh
@@ -1,32 +1,10 @@
 #!/bin/sh
 
-### function to retrieve property value ###
-getFileProperty () {
-file="$1"
-match_found=false
-input="$2"
-while IFS='=' read -r key value
-do
-if [ "$input" = "$key" ]
-then
-   eval input=\${value}
-   match_found=true
-   break
-fi   
-done < "$file"
-if [ $match_found = true ]
-then
-  echo "$input"
-else
-  echo "no_match_found_$input"  
-fi	 
-}
-
-### jenkins working directory ###
 wrkdir=${PWD}
 echo "$wrkdir"
 list=`ls -lrt $wrkdir`
 wrkdir="$(echo $wrkdir | sed 's/\/var\/jenkins_home\// /g')"
 echo "$wrkdir"
 echo "$list"
-docker run -v jenkins_home_volume:/workdir -w /e2e/$wrkdir cypress/included:12.5.1
+docker run -v jenkins_home_volume:/e2e -w /e2e/$wrkdir cypress/included:10.10.0
+

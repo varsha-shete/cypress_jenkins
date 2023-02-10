19916f9d8800d79c87849cccce4e4cd93ab6149c
Shete <varsha-vishwas.shete@capgemini.com>
2023-02-10 20:48:22 +0530

    jenkins

--- utilities/script.sh
+++ utilities/script.sh
@@ -6,5 +6,5 @@ echo "$wrkdir"
 ls -lirt
 wrkdir="$(echo $wrkdir | sed 's/\/var\/jenkins_home\///g')"
 echo "$wrkdir"
-docker run -v jenkins_home_volume:/e2e -w /e2e/$wrkdir cypress/included:10.10.0 --user $(whoami)
+docker run -v jenkins_home_volume:/e2e -w /e2e/$wrkdir cypress/included:10.10.0
 ls -lrt

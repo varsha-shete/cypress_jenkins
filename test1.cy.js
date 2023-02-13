87fb50226eb661f1ac87656e132184b4a2d19ee2
Shete <varsha-vishwas.shete@capgemini.com>
2023-02-13 10:34:18 +0530

    jenkins

--- test1.cy.js
+++ test1.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example1.cypress.io')
-  })
+import 'cypress-soft-assertions'
+it('assetion demo',() => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('get').click()
+    cy.get('#query-btn')
+        .should('contain','Button')
+        .and('have.class','query-btn')
+        .and('be.visible')
+        .and('be.enabled')
+    expect(true).to.be.true
+    assert.equal(4,'4','NOT EQUAL')
 })

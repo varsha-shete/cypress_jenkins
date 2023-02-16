7b90faefddafa3f6b4f80fd2c665d8938baacd80
Shete <varsha-vishwas.shete@capgemini.com>
2023-02-16 20:11:33 +0530

    deleted package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,21 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  reporter: 'junit',
+ //reporter: 'junit',
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    mochaFile: 'results/my-test-output-[hash].xml',
-    toConsole: true,
+    //mochaFile: 'results/my-test-output-[hash].xml',
+    //toConsole: true,
+    reporter: 'cypress-mochawesome-reporter',
   },
   e2e: {
     baseUrl: 'https://docs.cypress.io/guides/references/configuration',
 	supportFile: false,
 	specPattern: '**/*.cy.js'
+	setupNodeEvents(on, config) {
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+    },
   },
 })

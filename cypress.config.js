4c6d3a6521a5e177aad46b21015f36eb8a38eb0e
Shete <varsha-vishwas.shete@capgemini.com>
2023-02-21 10:21:47 +0530

    junit and html report generation

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   "reporter": "cypress-multi-reporters",
   "video": false,
   "reporterOptions": {
-	  "reporterEnabled": "cypress-mochawesome-reporter, mocha-junit-reporter",
+	  "reporterEnabled": "cypress-mochawesome-reporter, junit",
 	  "mochaJunitReporterReporterOptions": {
       		"mochaFile": "cypress/reports/results-[hash].xml",
 		"jenkinsMode": true,

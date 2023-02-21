2279d29f5c73c8e1e85d8af8dfea45b231049796
Shete <varsha-vishwas.shete@capgemini.com>
2023-02-21 09:52:03 +0530

    junit and html report generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress')
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   //reporter: 'junit',
@@ -25,7 +26,21 @@ module.exports = defineConfig({
       supportFile: '**/support/e2e.js',
       specPattern: '**/*.cy.js',
       setupNodeEvents(on, config) {
-       require('cypress-mochawesome-reporter/plugin')(on);
+      // require('cypress-mochawesome-reporter/plugin')(on);
+	      on('before:run', async (details) => {
+
+        console.log('override before:run');
+
+        await beforeRunHook(details);
+
+      });
+	      on('after:run', async () => {
+
+        console.log('override after:run');
+
+        await afterRunHook();
+
+      });
      },	
   },
 })
